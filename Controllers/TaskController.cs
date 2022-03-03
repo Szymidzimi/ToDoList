6aef9a30e9dd2ee1a1d84cbf188d16d90d38d48d
Szymon Napieraj <szymon222333@wp.pl>
2022-03-03 15:59:42 +0100

    the basics of the project

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoManager.Models;
+
+namespace ToDoManager.Controllers
+{
+    public class TaskController : Controller
+    {
+        private static IList<TaskModel> tasks = new List<TaskModel>()
+        {
+            new TaskModel(){IdTask=1,Name="kup mleko",Description="UHT 3,2",Priority=5,Status=false},
+            new TaskModel(){IdTask=2,Name="kup mleko 2",Description="UHT 1,5",Priority=5,Status=false},
+            new TaskModel(){IdTask=3,Name="kup mleko 3",Description="UHT 0,5",Priority=5,Status=false}
+        };
+        // GET: TaskController
+        public ActionResult Index()
+        {
+            return View(tasks);
+        }
+
+        // GET: TaskController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TaskController/Create
+        public ActionResult Create()
+        {
+            return View(new TaskModel());
+        }
+
+        // POST: TaskController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(TaskModel taskModel)
+        {
+            taskModel.IdTask = tasks.Count + 1;
+            tasks.Add(taskModel);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: TaskController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TaskController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TaskController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TaskController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/TaskModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoManager.Models
+{
+    public class TaskModel
+    {
+        public int IdTask { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Priority { get; set; }
+        public bool Status { get; set; }
+
+    }
+}
